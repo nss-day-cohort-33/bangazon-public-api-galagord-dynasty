33131af60fdfa87815ab5c4592a87a1749bed167
Shane Miller <shanemiller89@gmail.com>
2019-10-07 17:46:32 -0500

    product dfix for create

--- bangazonapp/models/product.py
+++ bangazonapp/models/product.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 from .customer import Customer
 from .categorytype import CategoryType
 
@@ -9,9 +10,9 @@ class Product(models.Model):
     description = models.CharField(max_length=150)
     quantity = models.IntegerField()
     price = models.DecimalField(max_digits=7, decimal_places=2)
-    creation_date = models.DateField(auto_now_add=True)
+    creation_date = models.DateField(auto_now_add=True, blank=True)
     location = models.CharField(max_length=50)
-    image = models.ImageField(upload_to=".static/media")
+    image = models.ImageField(upload_to=".static/media", blank="True")
     category_type = models.ForeignKey(CategoryType, on_delete=models.DO_NOTHING, related_name="products")
     customer = models.ForeignKey(Customer, on_delete=models.DO_NOTHING, related_name="products")
 
