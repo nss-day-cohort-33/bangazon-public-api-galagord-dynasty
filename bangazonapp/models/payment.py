a802c35a87e7cf1acf0883541cf7c64071bdbf12
Karla Gallegos <kgalleg618@gmail.com>
2019-10-15 13:19:07 -0500

    added soft delete in payment models

--- bangazonapp/models/payment.py
+++ bangazonapp/models/payment.py
@@ -1,15 +1,18 @@
 from django.db import models
 from .customer import Customer
+from django.urls import reverse
+from safedelete.models import SafeDeleteModel
+from safedelete.models import SOFT_DELETE
 
 
-class Payment(models.Model):
-
+class Payment(SafeDeleteModel):
+    _safedelete_policy = SOFT_DELETE
     merchant_name = models.CharField(max_length=50)
     account_number = models.IntegerField()
     created_date = models.DateField(auto_now_add=True)
     expiration_date = models.DateField()
-    customer = models.ForeignKey(Customer, on_delete=models.DO_NOTHING, related_name="payment")
-    
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name="payment")
+
 
     class Meta:
         verbose_name = ("payment")
